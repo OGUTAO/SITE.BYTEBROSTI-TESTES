0e2476b5aff238a6bf7c50e65685b84b885b8110
Luiz Felipe Santos <126472482+OGUTAO@users.noreply.github.com>
2025-04-23 00:16:34 -0300

    Delete public directory

--- public/js/theme.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark-theme');
-    }
-    if (localStorage.getItem('fontSize')) {
-        document.documentElement.style.fontSize = localStorage.getItem('fontSize');
-    }
-});
